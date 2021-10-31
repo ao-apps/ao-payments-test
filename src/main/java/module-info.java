d4b0c879e4d3ae6c27f8210de67e2b8e2e497c46
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:55:09 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ao-payments-test.  If not, see <http://www.gnu.org/licenses/>.
+ * along with ao-payments-test.  If not, see <https://www.gnu.org/licenses/>.
  */
 module com.aoapps.payments.test {
 	exports com.aoapps.payments.test;
